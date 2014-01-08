be0ee589b63fa880649de0878e23fe6044b71e71
chihirokaasan <sena_mybaby@yahoo.co.jp>
2014-01-08 22:05:18 +0900

    added prepare

--- display_kind.php
+++ display_kind.php
@@ -2,24 +2,30 @@
 /*
 Categories are displayed on a widget. 
 */
-include 'bp-user-search.php';
+//include 'bp-user-search.php';
 global $wpdb;
 $parent = htmlspecialchars($par['parent']);
 $childs = htmlspecialchars($par['child']);
 
-$field = $wpdb->get_results( "SELECT * 
-FROM `wp_bp_xprofile_fields` 
-WHERE `name` LIKE '{$parent}'" );
+$field = $wpdb->get_results($wpdb->prepare("
+	SELECT * 
+	FROM `wp_bp_xprofile_fields` 
+	WHERE `name` LIKE '%s'
+", $parent));
 
-$kinds = $wpdb->get_results( "
-SELECT * 
-FROM `wp_bp_xprofile_fields` 
-WHERE `parent_id` = {$field[0]->id}" );
+$kinds = $wpdb->get_results($wpdb->prepare("
+	SELECT * 
+	FROM `wp_bp_xprofile_fields` 
+	WHERE `parent_id` = %d
+", $field[0]->id));
 
-$user_id = $wpdb->get_results( "SELECT * 
-FROM  `wp_bp_xprofile_data` 
-WHERE  `field_id` ={$field[0]->id}
-AND  `value` LIKE  '%{$_GET['category_name']}%'" );
+$user_id = $wpdb->get_results($wpdb->prepare("
+	SELECT * 
+	FROM  `wp_bp_xprofile_data` 
+	WHERE  `field_id` ={$field[0]->id}
+	AND  `value` LIKE  '%%s%'
+", $_GET['category_name']));
+
 echo '<h3 class="widgettitle">'.$parent.'</h3>';
  foreach ($user_id as $user_id) {
 	$include_user .= $user_id->user_id.',';
@@ -31,35 +37,43 @@ $include_user = substr($include_user, 0, -1);
 
 <?php foreach($kinds as $kind):?>
 <?php 
-$group = $wpdb->get_results( "
-SELECT * 
-FROM  `wp_bp_xprofile_groups` 
-WHERE  `name` LIKE  '{$kind->name}'" );
 
-$janle = $wpdb->get_results( "
-SELECT * 
-FROM  `wp_bp_xprofile_fields` 
-WHERE  `group_id` ={$group[0]->id}
-AND  `name` LIKE  '{$childs}'" );
+$group = $wpdb->get_results($wpdb->prepare("
+	SELECT * 
+	FROM  `wp_bp_xprofile_groups` 
+	WHERE  `name` LIKE  '%s'
+", $kind->name));
 
-$janle_child = $wpdb->get_results( "
-SELECT * 
-FROM  `wp_bp_xprofile_fields` 
-WHERE  `group_id` = {$janle[0]->group_id}
-AND `name` LIKE  '{$childs}'" );
+$janle = $wpdb->get_results($wpdb->prepare("
+	SELECT * 
+	FROM  `wp_bp_xprofile_fields` 
+	WHERE  `group_id` =%d
+	AND  `name` LIKE  '%s'
+", $group[0]->id,$childs));
 
-$kind_child = $wpdb->get_results( "
-SELECT * 
-FROM  `wp_bp_xprofile_fields` 
-WHERE  `parent_id` = {$janle_child[0]->id}
-" );
+$janle_child = $wpdb->get_results($wpdb->prepare("
+	SELECT * 
+	FROM  `wp_bp_xprofile_fields` 
+	WHERE  `group_id` = %d
+	AND `name` LIKE  '%s'
+", $janle[0]->group_id,$childs));
+
+if(!empty($janle_child[0]->id)){
+	$kind_child = $wpdb->get_results($wpdb->prepare("
+	SELECT * 
+	FROM  `wp_bp_xprofile_fields` 
+	WHERE  `parent_id` = %d
+", $janle_child[0]->id));
+}
+
 ?>
 <li><a href="<?php get_option('siteurl') ;?>/members/?category_id=<?php echo $field[0]->id;?>&category_name=<?php echo $kind->name;?>"><?php echo $kind->name;?></a>
+<?php if(isset($kind_child)):?>
 <ul>
 	<?php foreach($kind_child as $child):?>
 	<li><a href="<?php get_option('siteurl') ;?>/members/?category_id=<?php echo $janle_child[0]->id;?>&category_name=<?php echo $child->name;?>"><?php echo $child->name;?></a></li>
     <?php endforeach;?>
-
 </ul>
+<?php endif;?>
 </li>
 <?php endforeach;?>
